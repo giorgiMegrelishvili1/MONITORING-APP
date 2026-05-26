971d4164a0d0d19e7e693cc7f80330f3689f9d29
giorgiMegrelishvili1 <g.megrelishvili200@gmail.com>
2026-05-26 17:25:25 +0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-from scrapers.aversi import scrape_aversi
-from scrapers.gpc import scrape_gpc
-from scrapers.psp import scrape_psp
-
-__all__ = ["scrape_psp", "scrape_gpc", "scrape_aversi"]
+from aversi import scrape_aversi
+from gpc import scrape_gpc
+from psp import scrape_psp
+from common import normalize_key
